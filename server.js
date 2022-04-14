ebd78ff83781f9d4608249525e52ce46db27e3f6
Colin <colinhickey@mac.com>
2022-04-14 12:27:21 -0400

    completed, all tests pass

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require("multer");
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 var app = express();
 
@@ -11,10 +14,16 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+//API
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
